443e23400c5e4b187a089243facf32d1d1974bb7
aipatchouli <48445594+aipatchouli@users.noreply.github.com>
2020-01-22 23:51:17 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -10,7 +10,7 @@ deb-src http://security.debian.org/debian-security buster/updates main contrib
 EOF
 
 apt update && apt full-upgrade -y
-apt install git wget curl gcc make lrzsz screen htop g++ cmake -y
+apt install git wget curl gcc make lrzsz screen glances g++ gcc cmake neofetch -y
 
 wget --no-check-certificate -q -O bbr2.sh "https://raw.githubusercontent.com/yeyingorg/bbr2.sh/master/bbr2.sh" && chmod +x bbr2.sh && bash bbr2.sh auto
 
